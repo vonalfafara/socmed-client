40002dce8b8fab80fc84a1f0a1015d5bb921cb8e
vsalfafara <alfafara.vm@gmail.com>
2023-10-16 13:31:31 +0800

    removed csrf

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -29,10 +29,6 @@ const Login = () => {
   });
 
   useEffect(() => {
-    async function getCsrf() {
-      await api.get(`${import.meta.env.VITE_ROOT}/sanctum/csrf-cookie`);
-    }
-    getCsrf();
     return () => {};
   }, []);
 
